772773b9b1e070bb2a2d1baea597bab0d2db27c7
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-06-13 16:59:45 -0500

    More changes

--- micro_aeth.rb
+++ micro_aeth.rb
@@ -1,32 +1,69 @@
 require 'serialport'
 
-class Message
-  attr :original_char_string,
-       :ref,
-       :sen1,
-       :sen2,
-       :flow,
-       :pcb_temp,
-       :time,
-       :status,
-       :battery
-##
-# data - bytes of data passed as a char string
-  def initialize data
-    @original_char_string = data
-    b = data.bytes
+module MicroAeth
 
-    @ref = b[0..2]
-    @sen1 = b[3..5]
-    @sen2 = b[6..8]
-    @flow = b[9..10]
-    @pcb_temp = b[11]
-    @time = Time.new ('20' + b[12].to_s).to_i, b[13], b[14], b[15], b[16], b[17]
-    @status = b[18]
-    @battery = "#{b[19]}.#{b[20]}".to_f
+  class Message
+    attr :original_char_string,
+         :ref,
+         :sen1,
+         :sen2,
+         :flow,
+         :pcb_temp,
+         :time,
+         :status,
+         :battery
+  ##
+  # @param data [String] conents between the STX "\x02"
+  #   and the ETX "0x03"
+    def initialize data
+      raise "invalid data" unless validate_data data
+      @original_char_string = data
+      read_data( data[7..-1])
+    end
+
+    private
+      def validate_data d
+        d != nil
+      end
+      def read_data d
+        b = d.bytes
+        @ref = d[0..2].unpack("v")[0]
+        @sen1 = d[3..5].unpack("v")[0]
+        @sen2 = d[6..8].unpack("v")[0]
+        @flow = d[9..10].unpack("v")[0]
+        @pcb_temp = b[11]
+        @time = Time.new ('20' + b[12].to_s).to_i, b[13], b[14], b[15], b[16], b[17]
+        @status = b[18]
+        @battery = d[19..20].unpack("v")[0]
+      end
+  end
+    
+
+
+  class Com
+    attr_accessor :com
+
+    def initialize
+      port     = '/dev/ttyUSB1'
+      baud     = 500_000
+      bytesize = 8
+      stopbits = 1
+      parity   = SerialPort::MARK
+      @com     = SerialPort.new port, baud, bytesize, stopbits, parity
+    end
+
+    def read_message
+      message = ''
+      while @com.readchar != "\x02"
+        nil
+      end
+      while (c = @com.readchar) != "\x03"
+        message = message + c
+      end
+      Message.new message
+    end
   end
 end
-  
 
 =begin
   # from http://aethlabs.com/sites/all/content/microaeth/microAeth%20Model%20AE51%20Operating%20Manual.pdf
@@ -71,27 +108,3 @@ end
   - Data(index+29)
   - Data(index+30) 
 =end
-
-class MicroAeth
-  attr_accessor :com
-
-  def initialize
-    port     = '/dev/ttyUSB1'
-    baud     = 500_000
-    bytesize = 8
-    stopbits = 1
-    parity   = SerialPort::MARK
-    @com     = SerialPort.new port, baud, bytesize, stopbits, parity
-  end
-
-  def read_message
-    message = ''
-    while @com.readchar != "\x02"
-      nil
-    end
-    while (c = @com.readchar) != "\x03"
-      message = message + c
-    end
-    Message.new message[7..-1]
-  end
-end
