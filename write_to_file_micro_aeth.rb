c484134f59953547c25c70052f25e190e525fe95
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-07-09 15:49:17 -0500

    fixed the naming of the file

--- write_to_file_micro_aeth.rb
+++ write_to_file_micro_aeth.rb
@@ -2,7 +2,7 @@ require_relative 'micro_aeth.rb'
 
 com = MicroAeth::Com.new
 com.start
-file_name = 'tmp/' + Time.now.to_s + '.micro_aeth.dat'
+file_name = '/home/pi/Desktop/LiveGraphData/' + Time.now.to_s + '.micro_aeth.dat'
 f = File.new file_name, 'w'
 f << %w(Timedate Ref Sen ATN Flow Temp Status Battery sigma_ap).join(',') + "\n"
 f.close
