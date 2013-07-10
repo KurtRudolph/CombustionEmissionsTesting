8b9c432b50b8493d086fd82795307bba621815aa
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-07-10 14:59:04 -0500

    print out header when something it input other than exit

--- write_to_file_daq.rb
+++ write_to_file_daq.rb
@@ -5,13 +5,14 @@ daq = DAQ.new
 daq.start
 
 file_name = '/home/pi/Desktop/LiveGraphData/' + Time.now.to_s + '.daq.dat'
+header = "DateTime," + daq.column_names.join(',')
 f = File.new file_name, 'w'
-f << "DateTime," + daq.column_names.join(',') + "\n"
+f << header + "\n"
 f.close
 daq.start_write_to_file file_name
 
 begin
-  print "\nType 'exit' to stop program: "
+  print "\n#{header}\nType 'exit' to stop program: "
 end while gets != "exit\n"
 
 puts "\nStopping writes to #{f.path}"
--- write_to_file_micro_aeth.rb
+++ write_to_file_micro_aeth.rb
@@ -5,13 +5,14 @@ com = MicroAeth::Com.new
 com.start
 file_name = '/home/pi/Desktop/LiveGraphData/' + Time.now.to_s + '.micro_aeth.dat'
 f = File.new file_name, 'w'
-f << %w(Timedate Ref Sen ATN Flow Temp Status Battery sigma_ap).join(',') + "\n"
+header = %w(Timedate Ref Sen ATN Flow Temp Status Battery sigma_ap).join(',')
+f << header + "\n"
 f.close
 
 com.start_write_to_file file_name
 
 begin
-  print "\nType 'exit' to stop program: "
+  print "\n#{header}\nType 'exit' to stop program: "
 end while gets != "exit\n"
 
 puts "\nStopping writes to #{f.path}"
